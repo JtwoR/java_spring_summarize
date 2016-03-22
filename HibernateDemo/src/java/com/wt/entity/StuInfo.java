557f140607e3feb87f2ecf1db4fa9ffb57b62cb2
hohoTT <609029365@qq.com>
2016-03-22 11:03:41 +0800

    回顾hibernate的用法，创建一个连接mysql数据库，并插入数据的例子

---
+++ HibernateDemo/src/java/com/wt/entity/StuInfo.java
@@ -0,0 +1,37 @@
+package com.wt.entity;
+
+import java.io.Serializable;
+
+public class StuInfo implements Serializable{
+    
+    private Integer s_id;
+    
+    private String s_name;
+    
+    private String s_sex;
+
+    public Integer getS_id() {
+        return s_id;
+    }
+
+    public void setS_id(Integer s_id) {
+        this.s_id = s_id;
+    }
+
+    public String getS_name() {
+        return s_name;
+    }
+
+    public void setS_name(String s_name) {
+        this.s_name = s_name;
+    }
+
+    public String getS_sex() {
+        return s_sex;
+    }
+
+    public void setS_sex(String s_sex) {
+        this.s_sex = s_sex;
+    }
+    
+}
---
+++ HibernateDemo/src/java/com/wt/test/test.java
@@ -0,0 +1,63 @@
+package com.wt.test;
+
+import com.wt.entity.StuInfo;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Example;
+import org.hibernate.service.ServiceRegistry;
+
+public class test {
+    
+    public static void main(String[] args) {
+        
+        // 创建 configuration 解析配置文件
+        Configuration configuration = new Configuration().configure();
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        
+        
+        // 创建 SessionFactory 解析配置文件
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        
+        // 创建 session
+        Session session = sessionFactory.openSession();
+        // CRUD
+        
+        // 开启事务
+        Transaction transaction = session.beginTransaction();
+        
+        // save
+        StuInfo stu = new StuInfo();
+        stu.setS_name("test");
+        stu.setS_sex("boy");
+        
+        // 调用 session 中的方法
+        session.save(stu);
+        
+        // 提交事务
+        transaction.commit();
+        
+        qdu(session);
+        
+        // 关闭 session 
+        session.close();
+    }
+
+    private static void qdu(Session session) throws HibernateException {
+        Criteria criteria = session.createCriteria(StuInfo.class);
+        criteria.add(Example.create(new StuInfo()));
+        List<StuInfo> list = criteria.list();
+        for (StuInfo stuInfo : list) {
+            System.out.println("------------------------------------");
+            System.out.println("s_id----- " + stuInfo.getS_id());
+            System.out.println("s_name---- " + stuInfo.getS_name());
+            System.out.println("s_sex----- " + stuInfo.getS_sex());
+        }
+    }
+    
+}
